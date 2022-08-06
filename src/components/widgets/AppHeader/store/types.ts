59c2a50dc4c951dbc9551a7609c7413bdf8d5625
stormuke <fen1x@yandex.ru>
2022-08-06 23:20:48 +0300

    feat(AppHeader): стартовый билд с типизацией

---
+++ src/components/widgets/AppHeader/store/types.ts
@@ -0,0 +1,7 @@
+import { ReactElement } from 'react';
+
+export interface HeaderButton {
+  element: ReactElement;
+  key: string;
+  title: string;
+}
---
+++ src/components/widgets/AppHeader/ui/index.tsx
@@ -0,0 +1,53 @@
+import {
+  BurgerIcon,
+  ListIcon,
+  Logo,
+  ProfileIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import { FC, useMemo } from 'react';
+
+import { HeaderButton } from 'components/widgets/AppHeader';
+import styles from './styles.module.scss';
+
+export const AppHeader: FC = () => {
+  const buttons = useMemo<HeaderButton[]>(
+    () => [
+      { element: <BurgerIcon type="primary" />, title: 'Конструктор', key: '1' },
+      { element: <ListIcon type="secondary" />, title: 'Лента заказов', key: '2' },
+    ],
+    [],
+  );
+
+  return (
+    <section className={styles.header}>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerContainerButtons}>
+          {buttons.map((item) => (
+            <div
+              className={styles.headerContainerButtonsElement}
+              key={item.key}
+            >
+              {item.element}
+              <p className={`text text_type_main-default text_color_${item.key === '1' ? 'primary' : 'inactive'}`}>
+                {item.title}
+              </p>
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.headerLogo}>
+          <Logo />
+        </div>
+
+
+        <div className={styles.headerContainerButtonsElement}>
+          <ProfileIcon type="secondary" />
+          <p className="text text_type_main-default text_color_inactive">
+            Личный кабинет
+          </p>
+        </div>
+
+      </div>
+    </section>
+  );
+};
