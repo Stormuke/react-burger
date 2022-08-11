97dfd53ede3e951b1b9ab21ddb1b833ad742d458
stormuke <fen1x@yandex.ru>
2022-08-11 22:48:50 +0300

    squash(Cards)
    style(Cards): сделал курсор поинтером на карточке
    
    feat(Cards): добавил логику открытия попапа и установки компонента, добавил попап с описанием ингредиента

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,29 +1,51 @@
-import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import {
+  Counter,
+  CurrencyIcon,
+} from '@ya.praktikum/react-developer-burger-ui-components';
 import type { FC } from 'react';
-import { CardsProps } from 'types/types';
+import { BurgerIngredientsData, CardsProps } from 'types/types';
+import { useState } from 'react';
+import { PopupWithIngredientsDetails } from '../PopupWithIgredientsDetails/PopupWithIngredientsDetails';
+
 import styles from './styles.module.scss';
 
-export const Cards: FC<CardsProps> = ({ cards }) => (
-  <div className={styles.container}>
-    {cards.map((item) => (
-      <div className={styles.containerElement} key={item._id}>
-        <Counter count={1} size="default" />
-        <img
-          className={styles.containerElementImage}
-          src={item.image_large}
-          alt={item.name}
-        />
+export const Cards: FC<CardsProps> = ({ cards }) => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [card, setCard] = useState<BurgerIngredientsData | null>(null)
 
-        <div className={styles.containerElementPrice}>
-          <p className="text text_type_digits-default">{item.price}</p>
-          <CurrencyIcon type="primary" />
+  return (
+    <div className={styles.container}>
+      {cards.map((item) => (
+        <div
+          className={styles.containerElement}
+          onClick={() => {
+            setIsPopupOpen(true);
+            setCard(item)
+          }}
+          key={item._id}
+        >
+          <Counter count={1} size="default" />
+          <img
+            className={styles.containerElementImage}
+            src={item.image_large}
+            alt={item.name}
+          />
+
+          <div className={styles.containerElementPrice}>
+            <p className="text text_type_digits-default">{item.price}</p>
+            <CurrencyIcon type="primary" />
+          </div>
+
+          <p className="text text_type_main-default ">{item.name}</p>
         </div>
+      ))}
 
-        <p className="text text_type_main-default ">
-          {item.name}
-        </p>
-
-      </div>
-    ))}
-  </div>
-);
+      <PopupWithIngredientsDetails
+        isOpened={isPopupOpen}
+        onClose={() => setIsPopupOpen(false)}
+        card={card}
+        title='Детали ингредиента'
+      />
+    </div>
+  );
+};
