c0fca51d2b3cac591491917da297d1114c38ec9c
stormuke <fen1x@yandex.ru>
2022-08-11 22:47:07 +0300

    feat(BurgerConstructor): добавил попап со деталями заказа

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -6,56 +6,68 @@ import {
 } from '@ya.praktikum/react-developer-burger-ui-components';
 import type { FC } from 'react';
 import { BurgerConstructorProps } from 'types/types';
+import { useState } from 'react';
+import {
+  PopupWithOrderDetails
+} from '../PopupWithOrderDetails/PopupWithOrderDetails';
+
 
 import styles from './styles.module.scss';
 
+
 export const BurgerConstructor: FC<BurgerConstructorProps> = ({
   ingredients,
-}) => (
-  <section className={styles.container}>
-    <div className={styles.containerElement}>
-      <ConstructorElement
-        text={ingredients[0].name}
-        thumbnail={ingredients[0].image}
-        price={ingredients[0].price}
-        type="top"
-        isLocked
-      />
-    </div>
-    <div className={styles.containerItems}>
-      {ingredients.map(
-        (item) =>
-          item.type !== 'bun' && (
-            <div className={styles.containerItemsItem} key={item._id}>
-              <DragIcon type="secondary" />
-              <ConstructorElement
-                key={item._id}
-                text={item.name}
-                thumbnail={item.image}
-                price={item.price}
-              />
-            </div>
-          ),
-      )}
-    </div>
-    <div className={styles.containerElement}>
-      <ConstructorElement
-        text={ingredients[0].name}
-        thumbnail={ingredients[0].image}
-        price={ingredients[0].price}
-        type="bottom"
-        isLocked
-      />
-    </div>
+}) => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false)
 
-    <div className={styles.containerCheckout}>
-      <div className={styles.containerCheckoutPrice}>
-        <p className="text text_type_digits-medium">
-          {ingredients.reduce((acc, item) => acc + item.price, 0)}
-        </p>
-        <CurrencyIcon type="primary" />
+  return (
+    <section className={styles.container}>
+      <div className={styles.containerElement}>
+        <ConstructorElement
+          text={`${ingredients[0].name} (Верх)`}
+          thumbnail={ingredients[0].image}
+          price={ingredients[0].price}
+          type='top'
+          isLocked
+        />
       </div>
-      <Button>Оформить заказ</Button>
-    </div>
-  </section>
-);
+      <div className={styles.containerItems}>
+        {ingredients.map(
+          (item) =>
+            item.type !== 'bun' && (
+              <div className={styles.containerItemsItem} key={item._id}>
+                <DragIcon type='secondary' />
+                <ConstructorElement
+                  key={item._id}
+                  text={item.name}
+                  thumbnail={item.image}
+                  price={item.price}
+                />
+              </div>
+            ),
+        )}
+      </div>
+      <div className={styles.containerElement}>
+        <ConstructorElement
+          text={`${ingredients[0].name} (Низ)`}
+          thumbnail={ingredients[0].image}
+          price={ingredients[0].price}
+          type='bottom'
+          isLocked
+        />
+      </div>
+
+      <div className={styles.containerCheckout}>
+        <div className={styles.containerCheckoutPrice}>
+          <p className='text text_type_digits-medium'>
+            {ingredients.reduce((acc, item) => acc + item.price, 0)}
+          </p>
+          <CurrencyIcon type='primary' />
+        </div>
+        <Button onClick={() => setIsPopupOpen(true)}>Оформить заказ</Button>
+      </div>
+
+      <PopupWithOrderDetails isOpened={isPopupOpen} onClose={() => setIsPopupOpen(false)} title=''/>
+    </section>
+  )
+}
