24c5f1d2645ea25202789577bf768a5640f1183f
stormuke <fen1x@yandex.ru>
2022-09-18 22:11:30 +0300

    feat(rootReducer): добавил мидлвары в стор

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -2,13 +2,23 @@ import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { IngredientsStore, OrderStore } from 'services';
+import { rtkQueryErrorLogger } from '../utils/middlewares';
 
 const rootReducer = {
   ingredients: IngredientsStore.ingredientsReducer,
   order: OrderStore.orderReducer,
 };
 
-export const store = configureStore({ reducer: rootReducer });
+const middlewares = [rtkQueryErrorLogger];
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      immutableCheck: false,
+      serializableCheck: false,
+    }).concat(middlewares),
+});
 
 setupListeners(store.dispatch);
 
