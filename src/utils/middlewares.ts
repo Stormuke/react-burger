9583b47a2b38e7c0cbbbf1742a5389a62645f9d4
stormuke <fen1x@yandex.ru>
2022-09-18 22:12:03 +0300

    feat(middlewares): добавил централизованный обработчик ошибок

---
+++ src/utils/middlewares.ts
@@ -0,0 +1,21 @@
+import { isRejected, isRejectedWithValue } from '@reduxjs/toolkit';
+import type { Middleware } from '@reduxjs/toolkit';
+
+/**
+ * Централизованная мидлвара для обработки ошибок с асинхронных запросов
+ */
+export const rtkQueryErrorLogger: Middleware = () => (next) => (action) => {
+  try {
+    if (isRejected(action) || isRejectedWithValue(action)) {
+        console.groupCollapsed('rtkQueryErrorLogger'); // eslint-disable-line no-console
+        console.warn('We got a rejected action!'); // eslint-disable-line no-console
+        console.log(action); // eslint-disable-line no-console
+        console.groupEnd(); // eslint-disable-line no-console
+    }
+  } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+  } finally {
+    next(action);
+  }
+};
