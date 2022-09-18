99cd2b398a0f605eb641bd4ec4a9bae131046bb7
stormuke <fen1x@yandex.ru>
2022-09-18 21:30:27 +0300

    env.d.ts: добавил тип ошибки и эндпоинт

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,3 +3,10 @@
 declare type Callback = () => void;
 
 declare type TitleAndKey = { title: string; key: string | number };
+
+declare type AppError =
+  | string
+  | import('@reduxjs/toolkit').SerializedError
+  | null;
+
+declare type Endpoint = string;
