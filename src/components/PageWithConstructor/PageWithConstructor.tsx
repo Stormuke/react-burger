3d8ec81928ed8886489462571f63da19b7809777
stormuke <fen1x@yandex.ru>
2022-09-18 21:48:58 +0300

    feat(PageWithConstructor): удалил стейты, вынес пост запрос в стор

--- src/components/PageWithConstructor/PageWithConstructor.tsx
+++ src/components/PageWithConstructor/PageWithConstructor.tsx
@@ -1,45 +1,25 @@
 import type { FC } from 'react';
-import { useEffect, useState } from 'react';
 
 import { BurgerConstructor } from 'components/BurgerConstructor/BurgerConstructor';
 import { Cards } from 'components/Cards/Cards';
-import { AppHeader } from '../AppHeader/AppHeader';
-import { BurgerIngredients } from '../BurgerIngredients/BurgerIngredients';
+import { AppHeader } from 'components/AppHeader/AppHeader';
+import { BurgerIngredients } from 'components/BurgerIngredients/BurgerIngredients';
+
 import styles from './styles.module.scss';
-import { BurgerIngredientsData } from '../../types/types';
-import { BASE_URL } from '../../utils/constants';
 
-const PageWithConstructor: FC = () => {
-  const [isPending, setIsPending] = useState(true)
-  const [active, setActive] = useState('bun');
-  const [ingredients, setIngredients] = useState<BurgerIngredientsData[]>([])
-
-  useEffect(() => {
-    setIsPending(true);
-    fetch(BASE_URL)
-      .then(res => res.ok ? res.json() : Promise.reject(res.status))
-      .then(res => setIngredients(res.data))
-      .catch(err => console.log(`Произошла ошибка при запросе: ${err}`))
-      .finally(() => setIsPending(false))
-  }, [])
-
-  const cards = ingredients.filter((i) => i.type === active);
-
-  return (
-    isPending ? <p className="text text_type_main-large mt-40 ml-40">...Загрузка</p> :
-    <section className={styles.page}>
-      <AppHeader />
-      <div className={styles.pageContent}>
-        <p className="text text_type_main-large mt-10 mb-5">Соберите бургер</p>
-        <div className={styles.pageContainer}>
-          <BurgerIngredients  activeTab={setActive}>
-            <Cards cards={cards} />
-          </BurgerIngredients>
-          <BurgerConstructor  ingredients={ingredients} />
-        </div>
+const PageWithConstructor: FC = () => (
+  <section className={styles.page}>
+    <AppHeader />
+    <div className={styles.pageContent}>
+      <p className="text text_type_main-large mt-10 mb-5">Соберите бургер</p>
+      <div className={styles.pageContainer}>
+        <BurgerIngredients>
+          <Cards />
+        </BurgerIngredients>
+        <BurgerConstructor />
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 export default PageWithConstructor;
