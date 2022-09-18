c8ab5b46119b0d28a20d9231b1c7f767eacc1597
stormuke <fen1x@yandex.ru>
2022-09-19 00:03:51 +0300

    squash(Cards)
    chore(Cards): добавил стили контейнера, поправил верстку
    
    feat(Cards): пересобрал массив ингредиентов, добавил тайтлы по типам, добавил скролл по табам
    
    chore(Cards): убрал генерацию ключа

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,16 +1,31 @@
-import type { FC } from 'react';
+import type { FC, RefObject } from 'react';
 import { BurgerIngredientsData } from 'types/types';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { IngredientsDetails } from 'components/IgredientsDetails/IngredientsDetails';
 import { Modal } from 'components/Modal/Modal';
 import { useAppSelector } from 'services/rootReducer';
 import { IngredientsStore } from 'services';
 import { Card } from 'components/Card/Card';
-import { v4 } from 'uuid';
 
 import styles from './styles.module.scss';
 
+interface IngredientsArray {
+  data: BurgerIngredientsData[];
+  key: string;
+  ref: RefObject<HTMLDivElement>;
+  title: string;
+  type: string;
+}
+
 export const Cards: FC = () => {
+  /***************************************************
+   *                     Переменные
+   ***************************************************/
+  const bunRef = useRef<HTMLDivElement>(null);
+  const mainRef = useRef<HTMLDivElement>(null);
+  const sauceRef = useRef<HTMLDivElement>(null);
+  const ingredientsArray: IngredientsArray[] = [];
+
   /***************************************************
    *                     Стейты
    ***************************************************/
@@ -23,17 +38,91 @@ export const Cards: FC = () => {
   const { response, tab } = useAppSelector(
     IngredientsStore.allIngredientsSelectors,
   );
-  const cards = response.data.filter((i) => i.type === tab);
+
+  /***************************************************
+   *              Пересборка ингредиентов
+   ***************************************************/
+  const ingredients = response.data.reduce(
+    (acc, item) => {
+      switch (item.type) {
+        case 'bun':
+          acc.buns.push(item);
+          return acc;
+        case 'sauce':
+          acc.sauce.push(item);
+          return acc;
+        case 'main':
+          acc.main.push(item);
+          return acc;
+        default:
+          return acc;
+      }
+    },
+    { buns: [], main: [], sauce: [] } as {
+      buns: BurgerIngredientsData[];
+      main: BurgerIngredientsData[];
+      sauce: BurgerIngredientsData[];
+    },
+  );
+
+  ingredientsArray.push(
+    {
+      title: 'Булки',
+      data: ingredients.buns,
+      ref: bunRef,
+      key: '1',
+      type: 'bun',
+    },
+    {
+      title: 'Соусы',
+      data: ingredients.sauce,
+      ref: sauceRef,
+      key: '2',
+      type: 'sauce',
+    },
+    {
+      title: 'Ингредиенты',
+      data: ingredients.main,
+      ref: mainRef,
+      key: '3',
+      type: 'main',
+    },
+  );
+
+  /***************************************************
+   *                     Сайды
+   ***************************************************/
+  useEffect(() => {
+    if (tab === 'bun') {
+      bunRef.current?.scrollIntoView({ block: 'start', behavior: 'smooth' });
+    }
+    if (tab === 'main') {
+      mainRef.current?.scrollIntoView({ block: 'start', behavior: 'smooth' });
+    }
+    if (tab === 'sauce') {
+      sauceRef.current?.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+      });
+    }
+  }, [tab]);
 
   return (
     <div className={styles.container}>
-      {cards.map((item) => (
-        <Card
-          key={v4()}
-          item={item}
-          onClick={() => setIsPopupOpen(true)}
-          setIngredient={setCard}
-        />
+      {ingredientsArray.map((item) => (
+        <div key={item.key} ref={item.ref}>
+          <p className="text text_type_main-large mb-6">{item.title}</p>
+          <div className={styles.containerItems}>
+            {item.data.map((element) => (
+              <Card
+                key={element._id}
+                item={element}
+                onClick={() => setIsPopupOpen(true)}
+                setIngredient={setCard}
+              />
+            ))}
+          </div>
+        </div>
       ))}
 
       <Modal
