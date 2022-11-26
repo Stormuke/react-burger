74fda92b588077c534cd80daf6ce93017889d6f1
stormuke <fen1x@yandex.ru>
2022-11-26 13:47:19 +0300

    feat(services): добавил сторы авторизации и профиля

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,4 @@
 export * as IngredientsStore from 'services/ingredientsStore';
 export * as OrderStore from 'services/orderStore';
+export * as AuthStore from 'services/authStore'
+export * as CabinetStore from 'services/cabinetStore'
