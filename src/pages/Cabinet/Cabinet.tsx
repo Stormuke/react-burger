6e827d5db6f3ffc8de49fafb7fb64faf80560253
stormuke <fen1x@yandex.ru>
2022-11-27 17:00:21 +0300

    feat(Cabinet): добавил сабмит и ресет формы, добавил валидацию полей

--- src/pages/Cabinet/Cabinet.tsx
+++ src/pages/Cabinet/Cabinet.tsx
@@ -1,11 +1,14 @@
-import type { FC } from 'react';
+import type { FC, SyntheticEvent } from 'react';
 import type { Form, Navigation } from 'types/types';
 import { CabinetNavigation } from 'components/CabinetNavigation/CabinetNavigation';
-import { CabinetStore } from 'services';
-import { useAppSelector } from 'services/rootReducer';
+import { AuthStore, CabinetStore } from 'services';
+import { useAppDispatch, useAppSelector } from 'services/rootReducer';
 import { useCreateSliceActions } from 'utils/useCreateSliceActions';
-import { deleteCookie } from 'utils/cookie';
+import { deleteCookie, getCookie } from 'utils/cookie';
 import { CabinetUserForm } from 'components/CabinetUserForm/CabinetUserForm';
+import { useCallback, useEffect, useState } from 'react';
+import { isFulfilled, isRejected } from '@reduxjs/toolkit';
+import { Spinner } from 'utils/ui/Spinner/Spinner';
 
 import styles from './styles.module.scss';
 
@@ -22,35 +25,100 @@ const cabinetButtons: Navigation[] = [
 ];
 
 const Cabinet: FC = ({ children }) => {
-  const { inputs } = useAppSelector(CabinetStore.allCabinetSelector);
+  const dispatch = useAppDispatch();
+  const accessToken = getCookie('accessToken');
+  const refreshToken = getCookie('refreshToken');
 
-  const { handleInputValue } = useCreateSliceActions(
+  const [isChanged, setIsChanged] = useState(false);
+
+  const cabinet = useAppSelector(CabinetStore.cabinetSelector);
+
+  const { handleInputValue, resetForm } = useCreateSliceActions(
     CabinetStore.slice.actions,
   );
+
+
+  const handleSubmit = useCallback(
+    async (e: SyntheticEvent) => {
+      e.preventDefault();
+      const res = await dispatch(
+        CabinetStore.patchUserDataThunk({
+          url: 'auth/user',
+          cookie: accessToken as string,
+          body: {
+            ...cabinet.form,
+            password:
+              cabinet.form.password === '' ? undefined : cabinet.form.password,
+          },
+        }),
+      );
+
+      if (isRejected(res)) {
+        const refresh = await dispatch(
+          AuthStore.postAuthFormThunk({
+            endpoint: 'auth/token',
+            body: { token: refreshToken as string },
+          }),
+        );
+
+        if (isFulfilled(refresh)) {
+          dispatch(
+            CabinetStore.patchUserDataThunk({
+              url: 'auth/user',
+              cookie: accessToken as string,
+              body: {
+                ...cabinet.form,
+                password:
+                  cabinet.form.password === ''
+                    ? undefined
+                    : cabinet.form.password,
+              },
+            }),
+          );
+        }
+      }
+    },
+    [accessToken, refreshToken, cabinet, dispatch],
+  );
+
+  const handleReset = (e: SyntheticEvent): void => {
+    e.preventDefault();
+    resetForm();
+  };
 
   const cabinetInputs: Form[] = [
     {
       name: 'name',
-      value: inputs.name,
+      value: cabinet.form.name,
       type: 'text',
       key: '1',
       placeholder: 'Имя',
     },
     {
       name: 'email',
-      value: inputs.email,
+      value: cabinet.form.email,
       type: 'email',
       key: '2',
       placeholder: 'Логин',
     },
     {
       name: 'password',
-      value: '12345678',
+      value: cabinet.form.password,
       type: 'password',
       key: '3',
       placeholder: 'Пароль',
     },
   ];
+
+  useEffect(
+    () =>
+      setIsChanged(
+        cabinet.form.name !== cabinet.inputs.name ||
+          cabinet.form.email !== cabinet.inputs.email ||
+          cabinet.form.password !== cabinet.inputs.password,
+      ),
+    [cabinet.form, cabinet.inputs],
+  );
 
   return (
     <section className={styles.cabinet}>
@@ -58,12 +126,18 @@ const Cabinet: FC = ({ children }) => {
         {CabinetNavigation({ navigation: cabinetButtons })}
       </div>
       <div className={styles.cabinetInfo}>
-        {children ?? (
-          <CabinetUserForm
-            inputsArr={cabinetInputs}
-            handleInput={handleInputValue}
-          />
-        )}
+        {children ??
+          (cabinet.isPending ? (
+            <Spinner />
+          ) : (
+            <CabinetUserForm
+              inputsArr={cabinetInputs}
+              handleInput={handleInputValue}
+              handleSubmit={handleSubmit}
+              isChanged={isChanged}
+              handleReset={handleReset}
+            />
+          ))}
       </div>
     </section>
   );
