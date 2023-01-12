8e56d9b94ff6fa46799abb7a547194ff2ac5264b
stormuke <fen1x@yandex.ru>
2023-01-12 19:43:53 +0300

    chore(orderActions): добавил токен в запрос

--- src/services/orderStore/orderActions.ts
+++ src/services/orderStore/orderActions.ts
@@ -4,9 +4,11 @@ import { apiInstance } from 'utils/apiInstance';
 
 export const postOrderThunk = createAsyncThunk<
   Order,
-  { body: object; endpoint: Endpoint }
->('order/postOrder', async ({ endpoint, body }) => {
-  const { data } = await apiInstance.post(endpoint, body);
+  { body: object; cookie: string; endpoint: Endpoint }
+>('order/postOrder', async ({ endpoint, body, cookie }) => {
+  const { data } = await apiInstance.post(endpoint, body, {
+    headers: { authorization: `Bearer ${cookie}` },
+  });
 
   return data;
 });
